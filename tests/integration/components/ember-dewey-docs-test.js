4696be744889ca9d35854f1caa2983c750f4ec6b
Michael Carroll <juwara0@users.noreply.github.com>
2017-03-07 11:42:24 -0600

    update tests to not use deprecated describeComponent

--- tests/integration/components/ember-dewey-docs-test.js
+++ tests/integration/components/ember-dewey-docs-test.js
@@ -1,29 +1,24 @@
 import {expect} from 'chai'
-import {
-  describeComponent,
-  it
-} from 'ember-mocha'
 import hbs from 'htmlbars-inline-precompile'
+import {describe, it} from 'mocha'
 
-describeComponent(
-  'ember-dewey-docs',
-  'Integration: EmberDeweyDocsComponent',
-  {
-    integration: true
-  },
-  function () {
-    it('renders', function () {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.on('myAction', function(val) { ... });
-      // Template block usage:
-      // this.render(hbs`
-      //   {{#ember-dewey-docs}}
-      //     template content
-      //   {{/ember-dewey-docs}}
-      // `);
+import {integration} from 'dummy/tests/helpers/ember-test-utils/setup-component-test'
 
-      this.render(hbs`{{ember-dewey-docs}}`)
-      expect(this.$()).to.have.length(1)
-    })
-  }
-)
+const test = integration('ember-dewey-docs')
+describe(test.label, function () {
+  test.setup()
+
+  it('renders', function () {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Template block usage:
+    // this.render(hbs`
+    //   {{#ember-dewey-docs}}
+    //     template content
+    //   {{/ember-dewey-docs}}
+    // `);
+
+    this.render(hbs`{{ember-dewey-docs}}`)
+    expect(this.$()).to.have.length(1)
+  })
+})
--- tests/integration/components/property-control-test.js
+++ tests/integration/components/property-control-test.js
@@ -1,29 +1,24 @@
 import {expect} from 'chai'
-import {
-  describeComponent,
-  it
-} from 'ember-mocha'
 import hbs from 'htmlbars-inline-precompile'
+import {describe, it} from 'mocha'
 
-describeComponent(
-  'property-control',
-  'Integration: PropertyControlComponent',
-  {
-    integration: true
-  },
-  function () {
-    it('renders', function () {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.on('myAction', function(val) { ... });
-      // Template block usage:
-      // this.render(hbs`
-      //   {{#property-control}}
-      //     template content
-      //   {{/property-control}}
-      // `);
+import {integration} from 'dummy/tests/helpers/ember-test-utils/setup-component-test'
 
-      this.render(hbs`{{property-control}}`)
-      expect(this.$()).to.have.length(1)
-    })
-  }
-)
+const test = integration('property-control')
+describe(test.label, function () {
+  test.setup()
+
+  it('renders', function () {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Template block usage:
+    // this.render(hbs`
+    //   {{#property-control}}
+    //     template content
+    //   {{/property-control}}
+    // `);
+
+    this.render(hbs`{{property-control}}`)
+    expect(this.$()).to.have.length(1)
+  })
+})
