6cf755b13b0b730b0b73b318a9701a5c238759c7
Michael Carroll <juwara0@users.noreply.github.com>
2017-07-11 10:17:22 -0500

    update test to register and inject the service

--- tests/integration/components/property-control-test.js
+++ tests/integration/components/property-control-test.js
@@ -1,12 +1,20 @@
 import {expect} from 'chai'
+import NotificationsService from 'ember-cli-notifications/services/notification-messages-service'
 import hbs from 'htmlbars-inline-precompile'
-import {describe, it} from 'mocha'
+import {beforeEach, describe, it} from 'mocha'
 
 import {integration} from 'dummy/tests/helpers/ember-test-utils/setup-component-test'
 
 const test = integration('property-control')
 describe(test.label, function () {
   test.setup()
+
+  beforeEach(function () {
+    this.register('service:notification-messages', NotificationsService)
+    this.inject.service('notification-messages', {as: 'notifications'})
+    // this.inject.service('notification-messages')
+    // this.render(hbs`{{property-control}}`)
+  })
 
   it('renders', function () {
     // Set any properties with this.set('myProperty', 'value');
